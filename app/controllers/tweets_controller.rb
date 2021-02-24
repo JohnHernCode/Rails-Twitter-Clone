317f74da9c37b2235ef6da8c6e787c4837ffb938
JohnHernCode <besthostandseoinfo@gmail.com>
2021-02-24 09:45:13 -0600

    Add User Model

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -26,7 +26,7 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to @tweet, notice: "Tweet was successfully created." }
+        format.html { redirect_to root_path, notice: "Tweet was successfully created." }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new, status: :unprocessable_entity }
